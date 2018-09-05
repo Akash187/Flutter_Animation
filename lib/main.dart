508e133cadd7b51b29a8aa9e391e0f45c75db48e
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-09-06 00:58:26 +0530

    Creating a counter Animation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'anim/counter_animation.dart';
 
 void main() => runApp(new MyApp());
 
@@ -25,32 +26,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
-  AnimationController controller;
-  Animation<double> animation;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    controller = new AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1800),
-    );
-    animation = new CurvedAnimation(parent: controller, curve: Curves.easeIn)..addListener((){
-      setState(() {
-        debugPrint("Running ${controller.value}");
-      });
-    });
-    controller.forward();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    controller.dispose();
-  }
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
@@ -59,14 +35,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         title: new Text(widget.title),
         centerTitle: true,
       ),
-      body: new Center(
-        child: Text(
-          "Hello World!",
-          style: TextStyle(
-            fontSize: 19.0 * animation.value
-          ),
-        ),
-      )
-    );
+      body: Center(child: new CounterAnimator())
+      );
   }
 }
